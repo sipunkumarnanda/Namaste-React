c2b531791678c6a3e8fcbf85ddd2d396dc785d5d
Sipun Kumar Nanda <98945228+sipunkumarnanda@users.noreply.github.com>
2025-05-18 18:18:57 +0000

    .

---
+++ 03. Igniting Our App/Code/dist/Code.6bd02f5a.js
@@ -0,0 +1,24 @@
+// let root = document.querySelector("#root")
+// let h1 = document.createElement("h1")
+// h1.textContent = "Namaste Everyone from JS !"
+// root.appendChild(h1)
+// import React from "react";
+// import ReactDOM from "react-dom"
+const heading1 = React.createElement("h1", {
+    id: "title"
+}, "Heading 1 !");
+// console.log(heading); 
+const heading2 = React.createElement("h2", {
+    id: "title-2"
+}, "Heading 2 ! ");
+const container = React.createElement("div", {
+    id: "container",
+    hello: "world"
+}, [
+    heading1,
+    heading2
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
+//# sourceMappingURL=Code.6bd02f5a.js.map
